a2d9484f40c2a227f5eaee594f6c3e7ebe7fcfe5
Rishavbro <kumarrishavsingh125@gmail.com>
2025-03-07 06:01:05 +0530

    fixed type error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Signup from './pages/Signup';
 import Blog from './pages/Blog';
 import Blogs from './pages/Blogs';
 import Publish from './components/Publish';
+import { Navigate } from 'react-router-dom';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blogs" element={<Blogs />} />
